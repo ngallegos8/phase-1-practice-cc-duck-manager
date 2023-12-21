ff6cf9661c05a4d232a500bd6abc731a5e82df8f
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-21 06:15:19 -0700

    solution for reference

--- src/index.js
+++ src/index.js
@@ -1,1 +1,119 @@
-// write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ELEMENTS
+// const duckNav = document.querySelector('#duck-nav')
+// const duckDisplayName = document.querySelector('#duck-display-name')
+// const duckDisplayImage = document.querySelector('#duck-display-image')
+// const duckDisplayLikes = document.querySelector('#duck-display-likes')
+// const newDuckForm = document.querySelector('#new-duck-form')
+
+// let currentDuck
+
+// // RENDER DUCK CARD FUNC
+// function renderDuckCard(duckObj) {
+//     const duckImg = document.createElement('img')
+//     duckImg.src = duckObj.img_url
+//     duckNav.append(duckImg)
+//     duckImg.addEventListener("click", () => {
+//         duckDisplayName.textContent = duckObj.name
+//         duckDisplayImage.src = duckObj.img_url
+//         duckDisplayLikes.textContent = `${duckObj.likes} likes`
+//         currentDuck = duckObj
+//     })
+// }
+
+// // ON LOAD FETCH
+// fetch("http://localhost:3000/ducks")
+// .then(res => res.json())
+// .then(ducksArray => ducksArray.forEach(renderDuckCard) )
+
+// // LIKE BUTTON EVENT
+// duckDisplayLikes.addEventListener("click", () => {
+//     if (currentDuck) {
+//         currentDuck.likes++
+//         duckDisplayLikes.textContent = `${currentDuck.likes} likes`
+//     }
+// })
+
+// // NEW DUCK SUBMIT EVENT
+// newDuckForm.addEventListener("submit", event => {
+//     event.preventDefault()
+//     const name = event.target["duck-name-input"].value
+//     const img_url = event.target["duck-image-input"].value
+
+//     const newDuck = { name, img_url, likes: 0 }
+
+//     renderDuckCard(newDuck)
+// })
