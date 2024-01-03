f8e96b4c89f0d873642d757e84025cf2a2513044
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-03 16:42:44 -0700

    all deliverables met

--- src/index.js
+++ src/index.js
@@ -1,60 +1,43 @@
+const duckNav = document.querySelector("#duck-nav")
+const duckDisplayName = document.querySelector("duck-display-name")
+const duckDisplayImage = document.querySelector("duck-display-image")
+const duckDisplayLikes = document.querySelector("duck-display-likes")
+const newDuckForm = document.querySelector("new-duck-form")
 
+let currentDuck
 
+function renderDuckCard(duckObj) {
+    const duckImg = document.createElement("img")
+    duckImg.src = duckObj.img_url
+    duckNav.append(duckImg)
+    duckImg.addEventListener("click", () => {
+        duckDisplayName.textContent = duckObj.name
+        duckDisplayImage.src = duckObj.img_url
+        duckDisplayLikes.textContent = `${duckObj.likes} likes`
+        currentDuck = duckObj
+    })
+}
 
+fetch("http://localhost:3000/ducks")
+    .then(res => res.json())
+    .then(ducksArray => ducksArray.forEach(renderDuckCard))
 
+duckDisplayLikes.addEventListener("click", () => {
+    if (currentDuck) {
+        currentDuck.likes++
+        duckDisplayLikes.textContent = `${currentDuck.likes} likes`
+    }
+})
 
+newDuckForm.addEventListener("submit", event => {
+    event.preventDefault()
+    const name = event.target["duck-name-input"].value
+    const img_url = event.target["duck-image-input"].value
 
+    const newDuck = { name, img_url, likes: 0 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    renderDuckCard(newDuck)
+})
 
 
 
